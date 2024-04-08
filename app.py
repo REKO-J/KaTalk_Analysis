52ccdfff999288d209e76d68614e48ba21600421
YeongHun Jo <117628869+REKO-J@users.noreply.github.com>
2024-04-08 11:00:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ current_dir = os.path.dirname(__file__)
 
 # 한글 폰트 설정
 font_path = os.path.join(current_dir, 'HMFMPYUN.TTF')
-font_name = font_manager.FontProperties(fname=font_path).get_name()
+font_name = fm.FontProperties(fname=font_path).get_name()
 plt.rc('font', family=font_name)
 
 
